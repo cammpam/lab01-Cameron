ff83b8c86cd484f1a599624e3fa6e1b0aba4449b
Cameron Pam <cammpam.creative@gmail.com>
2017-09-13 12:55:56 -0700

    Execeptions Added

--- lab01-Cameron/Program.cs
+++ lab01-Cameron/Program.cs
@@ -11,6 +11,7 @@ namespace lab01_Cameron
 
         static void Main(string[] args)
         {
+            int total = 0;
             Console.WriteLine("Enter Name to Start: ");
             string userInput = Console.ReadLine();
             if (string.IsNullOrEmpty(userInput))
@@ -21,32 +22,100 @@ namespace lab01_Cameron
             {
                 Console.WriteLine($"Welcome {userInput}");
             }
-            Console.WriteLine("Which ONE of these places as Cameron NOT lived: ");
-            Places();
-            Console.Read();
+
+            total += Places();
+            total += Cars();
+            total += Interest();
+            Console.WriteLine($"total score: {total}");
+            Console.ReadLine();
         }
-        static void TryAgain()
+        static int Places()
         {
-            for (int i = 0; i < 2; i++);
-        }
-        static void Places()
-        {
-            string[] placeArray = new string[] { "TX", "WA", "MS", "OK", "OR", "KY" };
+            int correct = 0;
+            Console.WriteLine("Which ONE of these places have I NOT lived: ");
+
+            string[] placeArray = new string[] { " A - Texas", " B - Washington", " C - Mississippi", " D - Oklahoma", " E - Oregon", " F - Kentucky" };
             foreach(string place in placeArray)
             {         
             Console.WriteLine(place);
             }
-            string answerOne = Console.ReadLine();
-            if (answerOne == "OK")
-            {
-                Console.WriteLine("Correct! Move to next question");
-            }
-            if (answerOne != "OK")
-            {
-                Console.WriteLine("Sorry, Try again.");
-                TryAgain();                
-            }
+            string answerOne = Console.ReadLine().ToUpper();
 
+            try
+            {
+                    if (answerOne == "d")
+                {
+                    Console.WriteLine("Correct!");
+                    correct = 1;
+                }
+                if(answerOne == "a" || answerOne == "b" || answerOne == "c")
+                {                    
+                    Console.WriteLine("Actually, I've never lived in Oklahoma.");                   
+                }
+                else
+                {
+                    throw new Exception();                 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("INVALID INPUT!" + e.Message);
+            }
+            finally
+            {
+                Console.Write("**exit**");
+            }
+            return correct;
+
+        }
+        static int Cars()
+        {
+            Console.WriteLine("What is the 3rd vehicle I Owned?");
+            string[] carArray = new string[] { " A - Monte Carlo", " B - Malibu", " C - Grand Prix", " D - Passat", " E - Fiesta" };
+            foreach(string car in carArray)
+            {
+                Console.WriteLine(car);
+            }
+            string answerTwo = Console.ReadLine();
+            if (answerTwo == "c" || answerTwo == "C")
+            {
+                Console.WriteLine("Correct!");
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Nope! My 3rd vehicle was a Grand Prix");
+                return 0;
+
+            }
+        }
+        static int Interest()
+        {
+            int attempts = 3;
+            int correct = 0;
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("What am I interested in: SELECT 3");
+                Console.WriteLine($"Attempt: {attempts}/3");
+                string[] interestArray = new string[] { " A - Music", " B - Finances", " C - Business", " D - Politics"};
+                foreach(string hobby in interestArray)
+                {
+                    Console.WriteLine(hobby);
+                }
+                string input = Console.ReadLine().ToUpper();
+ 
+                if (input == "ABC" || input == "ACB" || input == "BAC" || input == "BCA" || input == "CAB" || input == "CBA")
+                {
+                    Console.WriteLine("You are Correct! Thanks for Playing.");
+                    correct = 1;
+                    break;
+                }
+                else
+                {
+                        attempts--;
+                }
+            }
+            return correct;
         }
     }
 }
